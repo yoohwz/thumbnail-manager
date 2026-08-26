10c450764db9fc8efd39c55b33ec91a1f32357dd
YoOhw Studio <152001663+yoohwz@users.noreply.github.com>
2026-08-26 12:01:36 +0700

    ci: remove SVN dependency from WordPress tests

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -12,7 +12,7 @@ WP_TESTS_DIR="${WP_TESTS_DIR-/tmp/wordpress-tests-lib}"
 
 download() {
 	if command -v curl >/dev/null 2>&1; then
-		curl -sSL "$1" -o "$2"
+		curl -fsSL "$1" -o "$2"
 	else
 		wget -nv -O "$2" "$1"
 	fi
@@ -32,17 +32,38 @@ fi
 
 if [[ ! -d "$WP_TESTS_DIR/includes" ]]; then
 	mkdir -p "$WP_TESTS_DIR"
+	archive="$(mktemp)"
+	tmp_dir="$(mktemp -d)"
+
 	if [[ "$WP_VERSION" == "latest" ]]; then
-		tests_tag="trunk"
+		develop_archive="https://github.com/WordPress/wordpress-develop/archive/refs/heads/trunk.tar.gz"
 	else
-		tests_tag="tags/${WP_VERSION}"
+		develop_archive="https://github.com/WordPress/wordpress-develop/archive/refs/tags/${WP_VERSION}.tar.gz"
 	fi
-	svn export --quiet "https://develop.svn.wordpress.org/${tests_tag}/tests/phpunit/includes/" "$WP_TESTS_DIR/includes"
-	svn export --quiet "https://develop.svn.wordpress.org/${tests_tag}/tests/phpunit/data/" "$WP_TESTS_DIR/data"
+
+	download "$develop_archive" "$archive"
+	tar -C "$tmp_dir" -zxf "$archive"
+	develop_root="$(find "$tmp_dir" -mindepth 1 -maxdepth 1 -type d -print -quit)"
+
+	if [[ -z "$develop_root" || ! -d "$develop_root/tests/phpunit/includes" || ! -d "$develop_root/tests/phpunit/data" ]]; then
+		echo "Could not locate the WordPress PHPUnit test library for ${WP_VERSION}." >&2
+		rm -rf "$tmp_dir" "$archive"
+		exit 1
+	fi
+
+	cp -R "$develop_root/tests/phpunit/includes" "$WP_TESTS_DIR/includes"
+	cp -R "$develop_root/tests/phpunit/data" "$WP_TESTS_DIR/data"
+	rm -rf "$tmp_dir" "$archive"
 fi
 
 if [[ ! -f "$WP_TESTS_DIR/wp-tests-config.php" ]]; then
-	download "https://develop.svn.wordpress.org/trunk/wp-tests-config-sample.php" "$WP_TESTS_DIR/wp-tests-config.php"
+	if [[ "$WP_VERSION" == "latest" ]]; then
+		tests_ref="trunk"
+	else
+		tests_ref="$WP_VERSION"
+	fi
+
+	download "https://raw.githubusercontent.com/WordPress/wordpress-develop/${tests_ref}/wp-tests-config-sample.php" "$WP_TESTS_DIR/wp-tests-config.php"
 	sed -i.bak "s/youremptytestdbnamehere/${DB_NAME}/" "$WP_TESTS_DIR/wp-tests-config.php"
 	sed -i.bak "s/yourusernamehere/${DB_USER}/" "$WP_TESTS_DIR/wp-tests-config.php"
 	sed -i.bak "s/yourpasswordhere/${DB_PASS}/" "$WP_TESTS_DIR/wp-tests-config.php"
